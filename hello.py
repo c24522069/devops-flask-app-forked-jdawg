19025f1650c4fde7148a351807019c3b60980235
Jordan Corrigan <C24405302@mytudublin.ie>
2026-09-25 12:06:14 +0100

    changed text in new_greeting

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def say_hello():
     return f'''
-        <p>Hello, World, I am a Flask app!</p>
+        <p>Different string!</p>
         <a href="{url_for('about')}">About</a> |
         <a href="{url_for('contact')}">Contact</a>
     '''
